b72db728c654ee7dcfaf03171d89b32d8c1ee742
Chad Ellison <ChadEllison@Erins-iMac.local>
2018-09-29 14:30:58 -0500

    initial commit

---
+++ lib/server.rb
@@ -0,0 +1,25 @@
+require 'socket'
+
+
+module Rubbis
+  class Server
+    attr_reader :port
+
+    def initialize(port)
+      @port = port
+    end
+
+    def listen
+      socket = TCPServer.new(port)
+      loop do
+        handle_client(socket.accept)
+      end
+    end
+
+    def handle_client(client)
+      client.write "+OK\r\n"
+    ensure
+      client.close
+    end
+  end
+end
