98251347cb68a29aef508da290c1e019bf86406a
Chad Ellison <chad.ellison0123@gmail.com>
2018-11-02 12:12:01 -0500

    Add logarithmic sorted sets.

--- lib/rubbis/state.rb
+++ lib/rubbis/state.rb
@@ -162,25 +162,56 @@ module Rubbis
     end
 
     class ZSet
+      attr_reader :entries_to_score, :sorted_by_score
+
       def initialize
-        @entries = []
+        @entries_to_score = {}
+        @sorted_by_score = []
       end
 
       def add(score, member)
-        @entries << [score, member]
-        @entries.sort!
+        entries_to_score[member] = score
+        elem = [score, member]
+        index = bsearch_index(sorted_by_score, elem)
+        sorted_by_score.insert(index, elem)
       end
 
       def range(start, stop)
-        @entries[start..stop].map { |x| x[1] }
+        sorted_by_score[start..stop].map { |x| x[1] }
       end
 
       def rank(entry)
-        @entries.index { |x| x[1] == entry }
+        score = entries_to_score[entry]
+
+        return unless score
+
+        bsearch_index(sorted_by_score, [score, entry])
       end
 
       def score(entry)
-        @entries.detect { |x| x[1] == entry }[0]
+        entries_to_score[entry]
+      end
+
+      def bsearch_index(ary, x)
+        return 0 if ary.empty?
+
+        low = 0
+        high = ary.length - 1
+
+        while high >= low
+          idx = low + (high - low) / 2
+          comp = ary[idx] <=> x
+
+          if comp == 0
+            return idx
+          elsif comp > 0
+            high = idx - 1
+          else
+            low = idx + 1
+          end
+        end
+
+        idx + (comp < 0 ? 1 : 0)
       end
     end
 
